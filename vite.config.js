d324a9caf875229d406d8fc7f3507bf3fb4c7ea7
mbayestark <106484609+mbayestark@users.noreply.github.com>
2026-05-02 02:18:32 +0000

    Add PWA support for offline POS on iPad

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,woff,woff2}'],
+        navigateFallback: '/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts',
+              expiration: { maxEntries: 20, maxAgeSeconds: 60 * 60 * 24 * 365 },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: 'DaShop POS',
+        short_name: 'DaShop',
+        description: 'Life at DAUST Point of Sale',
+        theme_color: '#09090b',
+        background_color: '#09090b',
+        display: 'standalone',
+        start_url: '/pos',
+        scope: '/',
+        icons: [
+          {
+            src: '/favicon.png',
+            sizes: '498x498',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+})
